bd9000dd33fc757eb2b1fb1f68674c617746ba04
Brendan Fractalman <brendanvk@gmail.com>
2014-05-07 20:41:03 -0400

    Added Mask function

--- ExtensibleTileGame/public_html/js/game/Extension/Extension.js
+++ ExtensibleTileGame/public_html/js/game/Extension/Extension.js
@@ -12,10 +12,27 @@ var Extension = (function() {
         req.send();
     };
     
+    /**
+     * "Masks" an object behind a public set of methods
+     * @param {Object} obj The object to be masked behind this object
+     * @param {Array} visible A list of method names that are visible in obj
+     * @returns {Mask} A mask for obj, having functions in visible accessible
+     */
+    function Mask(obj, visible) {
+        for(var i = 0; i < visible.length; i ++) {
+            var name = visible[i];
+            if (typeof obj[name] === "undefined") {
+                this[name] = (function() {
+                    obj[visible[key]].apply({}, arguments);
+                });
+            }
+        }
+    }
+    
     module.prototype.load = function(tileRegistry, terrainGenerator) {
         get(this.src, function(e) {
-            this.extension = eval.call({}, e.target.responseText,
-                    tileRegistry, terrainGenerator);
+            this.extension = new Function(e.target.responseText).call(
+                    {}, tileRegistry, terrainGenerator);
         });
     };
     
