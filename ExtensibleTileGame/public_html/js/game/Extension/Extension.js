3097dfc33095fc6755d5189eb6dc3ddd22f3730f
Brendan Fractalman <brendanvk@gmail.com>
2014-05-07 20:48:55 -0400

    Added JsDoc comments

--- ExtensibleTileGame/public_html/js/game/Extension/Extension.js
+++ ExtensibleTileGame/public_html/js/game/Extension/Extension.js
@@ -1,8 +1,18 @@
 var Extension = (function() {
+    /**
+     * @constructor
+     * @param {string} src - The URI of the extension source file
+     * @returns {Extension} The newly-created {@linkcode Extension}
+     */
     var module = function(src) {
         this.src = src;
     };
     
+    /**
+     * 
+     * @param {strnig} src
+     * @param {function} callback
+     */
     function get(src, callback) {
         var req = new XMLHttpRequest();
         
@@ -14,8 +24,10 @@ var Extension = (function() {
     
     /**
      * "Masks" an object behind a public set of methods
-     * @param {Object} obj The object to be masked behind this object
-     * @param {Array} visible A list of method names that are visible in obj
+     * @constructor
+     * @param {Object} obj - The object to be masked behind this object
+     * @param {Array} visible - A list of method names that are visible in 
+     *      {@linkcode this.src}
      * @returns {Mask} A mask for obj, having functions in visible accessible
      */
     function Mask(obj, visible) {
@@ -28,7 +40,13 @@ var Extension = (function() {
             }
         }
     }
-    
+    /**
+     * Loads this extension, running the coed at this.src
+     * @param {TileRegistry} tileRegistry - Reference to the TileRegistry the
+     *      extension will load into
+     * @param {TerrainGenerator} terrainGenerator - Reference to the
+     *      TerrainGenerator the extension will load into
+     */
     module.prototype.load = function(tileRegistry, terrainGenerator) {
         get(this.src, function(e) {
             this.extension = new Function(e.target.responseText).call(
